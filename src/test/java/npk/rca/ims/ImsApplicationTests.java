8f14c06a291558749a0ea03f53001bbc00f94790
Ntarekp <kpntare@gmail.com>
2025-11-24 16:50:56 +0200

    Added initial files for the IMS

---
+++ src/test/java/npk/rca/ims/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package npk.rca.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
