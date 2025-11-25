09aca7be9e68a2ed6c09327515496a4bc1eb9020
Ntarekp <kpntare@gmail.com>
2025-11-25 22:58:01 +0200

    Added comments to files

--- src/main/java/npk/rca/ims/model/TransactionType.java
+++ src/main/java/npk/rca/ims/model/TransactionType.java
@@ -1,10 +1,21 @@
 package npk.rca.ims.model;
 
+/**
+ * TransactionType Enum
+ *
+ * Represents the two types of stock movements:
+ * - IN: Stock coming into inventory (purchases, donations)
+ * - OUT: Stock leaving inventory (usage, consumption)
+ *
+*/
 public enum TransactionType {
-
-// Stock coming IN (purchases, donations, returns)
+    /**
+     * Stock coming IN (purchases, donations, returns)
+     */
     IN,
-//    Stock going OUT (usage, consumption, wastage)
-    OUT,
-}
 
+    /**
+     * Stock going OUT (usage, consumption, wastage)
+     */
+    OUT
+}
