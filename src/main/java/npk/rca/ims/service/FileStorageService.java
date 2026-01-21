348c78ad69c80c242207d5230d0309f5f1e5c129
Ntarekp <kpntare@gmail.com>
2026-01-21 08:35:43 +0200

    resolved merge conflicts
    # Conflicts:
    #	src/main/java/npk/rca/ims/service/FileStorageService.java

--- src/main/java/npk/rca/ims/service/FileStorageService.java
+++ src/main/java/npk/rca/ims/service/FileStorageService.java
@@ -1,7 +1,5 @@
 package npk.rca.ims.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -20,7 +18,6 @@ import java.util.UUID;
 @Service
 public class FileStorageService {
 
-    private static final Logger log = LoggerFactory.getLogger(FileStorageService.class);
     private final Path fileStorageLocation;
 
     public FileStorageService(@Value("${app.file.upload-dir:uploads}") String uploadDir) {
@@ -28,9 +25,7 @@ public class FileStorageService {
         try {
             Files.createDirectories(this.fileStorageLocation);
         } catch (Exception ex) {
-            log.error("Could not create the directory where the uploaded files will be stored: {}", this.fileStorageLocation, ex);
-            // We don't throw exception here to prevent application startup failure.
-            // File uploads will fail at runtime, but other parts of the app will work.
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
         }
     }
 
@@ -42,11 +37,6 @@ public class FileStorageService {
         try {
             if (fileName.contains("..")) {
                 throw new RuntimeException("Sorry! Filename contains invalid path sequence " + fileName);
-            }
-
-            // Ensure directory exists before storing (in case it was deleted or initial creation failed)
-            if (!Files.exists(this.fileStorageLocation)) {
-                Files.createDirectories(this.fileStorageLocation);
             }
 
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
